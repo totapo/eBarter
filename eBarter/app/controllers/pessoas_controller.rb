676717c2e32bfe12b4d04ed3bc62753186d3ec0b
MarcosFMS <marcos-fms@live.com>
2017-06-01 14:10:13 -0300

    tela de login

--- eBarter/app/controllers/pessoas_controller.rb
+++ eBarter/app/controllers/pessoas_controller.rb
@@ -2,12 +2,15 @@ class PessoasController < ApplicationController
 	def new
 		@pessoa = Pessoa.new
 	end
-		
+
 	def show
 	end
-		
+
+	def login
+
+	end
+
 	def create
-		
 		@pessoa = Pessoa.new(pessoas_params)
 			if params.require(:pessoa)[:senha] == params[:senhaConfirma][:text]
 				if(!@pessoa.save)
@@ -15,9 +18,16 @@ class PessoasController < ApplicationController
 				else
 					redirect_to @pessoa
 				end
-			else 
+			else
 				showError "Senhas não conferem!"
 			end
+	end
+
+	def submitlogin
+		@pessoa = Pessoa.find_by email: login_params[:email], senha: login_params[:senha]
+		if @pessoa
+			session[:user_id] = @pessoa.id;
+		end
 	end
 
 	def showError(message)
@@ -26,8 +36,13 @@ class PessoasController < ApplicationController
 	end
 
 	private
+		def login_params
+			params.require(:pessoa).permit(:email, :senha)
+		end
+
+	private
 		def pessoas_params
 			params.require(:pessoa).permit(:nome, :email, :senha, :cidade, :estado, :pais)
 		end
-		
+
 end
--- eBarter/config/routes.rb
+++ eBarter/config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   resources :items
 	resources :pessoas
   root 'welcome#index'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
