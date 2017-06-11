f13b3b1bfecee5b3840faec3123d478fe1f51406
Marcos Felipe Mendes Santos <marcos-fms@live.com>
2017-06-11 16:33:39 -0300

    atualização do projeto

--- eBarter/app/controllers/home_controller.rb
+++
@@ -1,7 +0,0 @@
-class HomeController < ApplicationController
-  def index
-    if(!session[:id_usuario])
-      redirect_to login_path
-    end
-  end
-end
---
+++ eBarter/spec/controllers/propostas_controller_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe PropostasController, type: :controller do
+
+end
