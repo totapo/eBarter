c45a3a16f934a7ebe29017aff358090aa130af3b
totapo <matheusraveneziani@gmail.com>
2017-06-29 05:07:11 +0900

    corrigindo erro fatal na welcome/controller

--- eBarter/app/controllers/welcome_controller.rb
+++ eBarter/app/controllers/welcome_controller.rb
@@ -2,7 +2,8 @@ class WelcomeController < ApplicationController
   def index
     if(!session[:id_usuario])
       redirect_to login_path
+    else
+      redirect_to home_path
     end
-    redirect_to home_path
   end
 end
